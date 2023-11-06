e5ade943a8c4c93ee2d061681ab550e820409b45
Kristiks00 <shar-04@mail.ru>
2023-11-06 13:51:31 +0300

    random values generator

---
+++ random-source.h
@@ -0,0 +1,12 @@
+typedef struct RandomSourceOperations RandomSourceOperations;
+typedef struct
+{
+    double value;
+    int fd;
+    RandomSourceOperations *op;
+}RandomSource;
+struct RandomSourceOperations
+{
+    RandomSource *(*destroy)(RandomSource *);
+    double (*next)(RandomSource *);
+};
